79a70ab2b3a2b538da3fec02d7f886242ae9aaa7
NUPIGM <53999409+NUPIGM@users.noreply.github.com>
2024-01-10 19:17:06 +0800

    1

--- gasoline_price.js
+++ gasoline_price.js
@@ -4,22 +4,23 @@ const cheerio = require("cheerio");
 // 在请求中包含和保存服务端的 cookie
 // axios.defaults.withCredentials = true;
 // 发出 HTTP 请求以获取网页内容
-url_main = "https://cx.sinopecsales.com/yjkqiantai/core/main"
-url_get = "https://cx.sinopecsales.com/yjkqiantai/data/initMainData"//get
-url_post = "https://cx.sinopecsales.com/yjkqiantai/data/switchProvince"//post
-const axios = require('axios');
+url_main = "https://cx.sinopecsales.com/yjkqiantai/core/main";
+url_get = "https://cx.sinopecsales.com/yjkqiantai/data/initMainData"; //get
+url_post = "https://cx.sinopecsales.com/yjkqiantai/data/switchProvince"; //post
+const axios = require("axios");
 
 // 定义 POST 请求的数据
 const postData = {
-  key1: 'value1',
-  key2: 'value2'
+  key1: "value1",
+  key2: "value2",
 };
 
 // 发送 POST 请求
-axios.post(url_post, postData)
-  .then(response => {
-    console.log('Response:', response.data);
+axios
+  .post(url_post, postData)
+  .then((response) => {
+    console.log("Response:", response.data);
   })
-  .catch(error => {
-    console.error('Error:', error);
+  .catch((error) => {
+    console.error("Error:", error);
   });
